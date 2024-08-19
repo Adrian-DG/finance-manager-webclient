a8f122b37bc2480bd81f1e6c1bd6d66d37234894
Adrian-DG <adrian96guilamo@gmail.com>
2024-08-19 14:11:01 -0400

    stablish main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { LinkListComponent } from './shared/ui/link-list/link-list.component';
+import { IUrlOption } from './shared/models/Iurl-option.model';
 
 @Component({
 	selector: 'app-root',
@@ -24,6 +25,11 @@ export class AppComponent {
 	title = 'finance-manager-webclient';
 
 	isSidenavOpen = false;
+
+	options: IUrlOption[] = [
+		{ name: 'Accounts', url: '/accounts' },
+		{ name: 'Incomes', url: '/incomes' },
+	];
 
 	toogleSidenav() {
 		this.isSidenavOpen = !this.isSidenavOpen;
