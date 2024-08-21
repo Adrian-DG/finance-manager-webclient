ba82aa4329910b3ee084d3dc67acbe33d1661010
Adrian-DG <adrian96guilamo@gmail.com>
2024-08-21 18:35:45 -0400

    index auth page design implemented

--- src/app/auth/pages/index/index.component.ts
+++ src/app/auth/pages/index/index.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-index',
-  standalone: true,
-  imports: [],
-  templateUrl: './index.component.html',
-  styleUrl: './index.component.scss'
+	selector: 'app-index',
+	standalone: true,
+	imports: [MatCardModule, MatButtonModule, MatDividerModule, MatIconModule],
+	templateUrl: './index.component.html',
+	styleUrl: './index.component.scss',
 })
-export class IndexComponent {
-
-}
+export class IndexComponent {}
