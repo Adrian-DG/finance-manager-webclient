52cc70817cb708eea1e89efe2087c9f9a6e2e2e8
Adrian-DG <adrian96guilamo@gmail.com>
2024-09-26 19:21:18 -0400

    auth guard minor changes

--- src/app/auth/guards/authetication.guard.ts
+++ src/app/auth/guards/authetication.guard.ts
@@ -6,7 +6,6 @@ import { map } from 'rxjs';
 export const CheckIfAuthenticated = () => {
 	const $router = inject(Router);
 	const _authService = inject(AuthService);
-	const isAuthenticated = _authService.isAuthenticated$();
 	if (_authService.isAuthenticated$()) {
 		$router.navigate(['']);
 		_authService.showNotifySnackbar('Token invalid or expired');
